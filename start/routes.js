080287fcd4ba78c16e71f3c66d1b9977a979d898
Renan Machado <renan.mav@hotmail.com>
2019-06-06 18:39:52 -0300

    Add store to SessionController

--- start/routes.js
+++ start/routes.js
@@ -1,19 +1,5 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
 Route.get('/', () => {
@@ -21,3 +7,4 @@ Route.get('/', () => {
 })
 
 Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
