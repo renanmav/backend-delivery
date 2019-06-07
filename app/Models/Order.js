9aafb567c2f5b3f967b91b116c5dda7e02f274c0
Renan Machado <renan.mav@hotmail.com>
2019-06-07 17:31:48 -0300

    Map Order relationships

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -1,9 +1,15 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Order extends Model {
+  user () {
+    return this.belongsTo('App/Model/User')
+  }
+
+  products () {
+    return this.hasMany('App/Models/Product')
+  }
 }
 
 module.exports = Order
