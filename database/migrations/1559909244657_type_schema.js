f2a59d145743240a8d883253d6afb0c776f01ae0
Renan Machado <renan.mav@hotmail.com>
2019-06-07 09:17:37 -0300

    Create Type schema

---
+++ database/migrations/1559909244657_type_schema.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class TypeSchema extends Schema {
+  up () {
+    this.create('types', table => {
+      table.increments()
+      table.string('name', 254).notNullable()
+      table.text('description').notNullable()
+      table
+        .integer('time')
+        .unsigned()
+        .notNullable()
+      table.float('grade', 2).notNullable()
+      table
+        .integer('file_id')
+        .unsigned()
+        .references('id')
+        .inTable('files')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('types')
+  }
+}
+
+module.exports = TypeSchema
