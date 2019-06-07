faf7e2ff12a833f973bcf63c2a7a26a272d373bd
Renan Machado <renan.mav@hotmail.com>
2019-06-07 10:28:43 -0300

    User seed fix

--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -1,39 +1,43 @@
 'use strict'
 
+const User = use('App/Models/User')
 const Database = use('Database')
 
 class UserSeeder {
   async run () {
-    await Database.table('users').insert({
+    await User.create({
       name: 'Leonardo',
       email: 'leader_d20@tmnt.com',
       password: 'ih4te.raph'
     })
 
-    await Database.table('users').insert({
+    await User.create({
       name: 'Donatello',
       email: 'don.tfight@tmnt.com',
       password: '_science4thewin_'
     })
 
-    await Database.table('users').insert({
+    await User.create({
       name: 'Raphael',
       email: 'phael@tmnt.com',
       password: 'str0ng.asf_ck'
     })
 
-    await Database.table('users').insert({
+    await User.create({
       name: 'Michelangelo',
       email: 'notthatmickey@tmnt.com',
       password: 'pizza_And_nunchaku5'
     })
 
-    await Database.table('users').insert({
+    await User.create({
       name: 'Mestre Splinter',
       email: 'splinter@tmnt.com',
-      password: 'ninjutsu',
-      is_admin: true
+      password: 'ninjutsu'
     })
+
+    await Database.table('users')
+      .where('email', 'splinter@tmnt.com')
+      .update({ is_admin: true })
   }
 }
 
