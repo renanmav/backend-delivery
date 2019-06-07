5a4910cba647f35c4672616b849048310725ad42
Renan Machado <renan.mav@hotmail.com>
2019-06-07 16:41:26 -0300

    Create Size schema

---
+++ database/migrations/1559936235901_size_schema.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class SizeSchema extends Schema {
+  up () {
+    this.create('sizes', table => {
+      table.increments()
+      table
+        .string('name', 254)
+        .notNullable()
+        .unique()
+      table.decimal('price').notNullable()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+      table
+        .integer('file_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('files')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table
+        .integer('product_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('products')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('sizes')
+  }
+}
+
+module.exports = SizeSchema
