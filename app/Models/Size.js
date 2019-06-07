72992b579dabff03ec2ebbabab7deb6ffdd58982
Renan Machado <renan.mav@hotmail.com>
2019-06-07 16:48:34 -0300

    Map Size relationships

--- app/Models/Size.js
+++ app/Models/Size.js
@@ -1,9 +1,19 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Size extends Model {
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  file () {
+    return this.belongsTo('App/Models/File')
+  }
+
+  product () {
+    return this.belongsTo('App/Models/Product')
+  }
 }
 
 module.exports = Size
