c340d94e68a0d1c584ad90727c1fe03dcf471e3e
Renan Machado <renan.mav@hotmail.com>
2019-06-14 23:39:57 -0300

    Change timezone to UTC for timestamps

--- config/database.js
+++ config/database.js
@@ -32,7 +32,9 @@ module.exports = {
   sqlite: {
     client: 'sqlite3',
     connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+      filename: Helpers.databasePath(
+        `${Env.get('DB_DATABASE', 'development')}.sqlite`
+      )
     },
     useNullAsDefault: true,
     debug: Env.get('DB_DEBUG', false)
@@ -77,7 +79,8 @@ module.exports = {
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      database: Env.get('DB_DATABASE', 'adonis'),
+      timezone: 'utc'
     },
     debug: Env.get('DB_DEBUG', false)
   }
